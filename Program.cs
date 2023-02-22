c025bf9011a6de12e47515b4b97ce47a1e4c43e6
poojakhandale <poojakhandale2000@gmail.com>
2023-02-22 17:32:51 +0530

    wage for multiple companies

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Day10EmpWage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("UC_10 Solution -- Check wage for multiple companies");
+        }
+    }
+}
