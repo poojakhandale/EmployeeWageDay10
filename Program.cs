1be3f519d091f09148d6f289f07740cf8ce7d9f8
poojakhandale <poojakhandale2000@gmail.com>
2023-02-22 17:52:57 +0530

    program for multiple comanies

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EmployeeWage;
+
 namespace Day10EmpWage
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("UC_10 Solution -- Check wage for multiple companies");
+            EmpWageBuilder wipro = new EmpWageBuilder("wipro", 12, 3, 10);
+            EmpWageBuilder tcs = new EmpWageBuilder("TATA CONSULTANCY SERVICES", 10, 4, 21);
+
+            wipro.computeEmpWage();
+            Console.WriteLine(wipro.toString());
+            tcs.computeEmpWage();
+            Console.WriteLine(tcs.toString());
         }
     }
 }
+
+            //Console.WriteLine("UC_10 Solution -- Check wage for multiple companies");
+       
+    
